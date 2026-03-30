e3ca582797c71a9e45ab40607277a03a554c69ee
juniormaximiano <juniormaximianoadeilson@hotmail.com>
2026-03-30 16:58:37 -0300

    feat: setup inicial do subscription sentinel

---
+++ src/main/java/repository/AssinaturaRepository.java
@@ -0,0 +1,7 @@
+package repository;
+
+import model.Assinatura;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AssinaturaRepository  extends JpaRepository<Assinatura, Integer> {
+}
